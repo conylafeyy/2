d3c05c90a52f78a865a1c52f7bb369d24ebb4c8c
Kate A <ek.agapovaa@yandex.ru>
2021-08-12 19:47:58 +0300

    the first

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int moneyTransfer = 500_000_000;
+        int total = balance + moneyTransfer;
+        System.out.println(total);
+    }
+}
